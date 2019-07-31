297a1d3c4210245da8d5b9b4a8ae15de620b9ea8
Apple <apple@Apples-MacBook-Pro-64.local>
2019-07-31 16:20:35 -0400

    Iteration 0 Completed End of Day 1

---
+++ UNbias/RaceViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RaceViewController.swift
+//  UNbias
+//
+//  Created by Apple on 7/31/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class RaceViewController: UIViewController {
+
+    
+    @IBAction func article1(_ sender: Any) {
+        showSafariVC(for: "https://www.youtube.com/watch?v=cfwXUIwr9rY")
+    }
+    
+    func showSafariVC(for url: String) {
+        guard let url = URL(string: url) else {
+            //alert
+            return
+        }
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
